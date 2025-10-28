9ddb46c10d5c4504fe07e6c5ee2b809fdeae1ffd
Darío Hernandez <dario.hernandez.uisek.edu.ec>
2025-10-27 23:32:38 -0500

    Finalización de laboratorio 4

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(max_digits=6, decimal_places=4)
+    height = models.DecimalField(max_digits=6, decimal_places=4)
+
+    def __str__(self):
+        return self.name
+    
+class Entrenador(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    apellido = models.CharField(max_length=50, null=False)
+    nivel = models.CharField(max_length=30, null=False)
+    fecha_nacimiento = models.DateField(null=False)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"   
